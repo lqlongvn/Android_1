fcb80191e2e00fd8ea29a720081d1beb63ebeec7
Long <lqlongvn@gmail.com>
2021-08-27 21:22:51 +0700

    List view flutter (27-8-21)

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'ListViews',
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: Text('ListViews')),
+        body: BodyLayout(),
+      ),
+    );
+  }
+}
+
+class BodyLayout extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return _myListView(context);
+  }
+}
+
+// replace this function with the code in the examples
+Widget _myListView(BuildContext context) {
+  return ListView(
+    children: <Widget>[
+      ListTile(
+        leading: CircleAvatar(
+          backgroundImage: AssetImage('assets/sun.jpeg'),
+        ),
+        title: Text('Sun'),
+        subtitle: Text('93 million miles away'),
+        trailing: Icon(Icons.keyboard_arrow_right),
+      ),
+      ListTile(
+        leading: CircleAvatar(
+          backgroundImage: AssetImage('assets/moon.jpeg'),
+        ),
+        title: Text('Moon'),
+      ),
+      ListTile(
+        leading: CircleAvatar(
+          backgroundImage: AssetImage('assets/stars.jpeg'),
+        ),
+        title: Text('Star'),
+      ),
+    ],
+  );
+}
